b992b8c4b90d398a46d87653153bbacd66b6d532
Farkor123 <a.sadowski1@onet.eu>
2018-10-11 12:41:25 +0200

    Update parser py
    No dashes in args

--- src/Parser.py
+++ src/Parser.py
@@ -7,25 +7,23 @@ class Parser:
 
         self._parser = argparse.ArgumentParser(description='15 Puzzle solver')
 
-        self._parser.add_argument('-a', '--algorithm', choices=['bfs', 'dfs', 'manh', 'hamm'], required=True, type=str,
+        self._parser.add_argument('algorithm', type=str,
                                   help='Choose which algorithm to use\nbfs - breadth-first-search\ndfs - '
                                        'depth-first-serach\nmanh - A* search with manhattan metric\nhamm - A* '
                                        'algorithm with Hamming metric')
 
-        self._parser.add_argument('-i', '--input', type=str, required=True,
+        self._parser.add_argument('priority', type=str,
+                                  help='Specify priority of blank tile moves separated by spaces')
+
+        self._parser.add_argument('input', type=str,
                                   help='Specify path for input file')
 
-        self._parser.add_argument('-s', '--solution', type=str, required=True,
+        self._parser.add_argument('solution', type=str,
                                   help='Specify path for solution file')
 
-        self._parser.add_argument('-m', '--more_info', required=True, type=str,
+        self._parser.add_argument('stats', type=str,
                                   help='Specify path for more info file')
 
-        self._parser.add_argument('-r', '--recursion_depth', type=int, default=20,
-                                  help='Specify maximum depth of recursion')
-
-        self._parser.add_argument('-p', '--priority', choices=['U', 'D', 'L', 'R'], nargs=4, default=['U', 'L', 'D', 'R'],
-                                  help='Specify priority of blank tile moves separated by spaces')
 
     def parse(self):
         self._args = self._parser.parse_args()
@@ -39,11 +37,8 @@ class Parser:
     def get_solution(self):
         return self._args.solution
 
-    def get_more_info(self):
-        return self._args.more_info
-
-    def get_recursion_depth(self):
-        return self._args.recursion_depth
+    def get_stats(self):
+        return self._args.stats
 
     def get_priority(self):
         return self._args.priority
